5286e6250833fc759ed9648b837a2875f1062836
ao627515 <ao627515@gmail.com>
2025-10-12 23:52:23 +0000

    Mise à jour des factories et du seeder pour les rôles : ajout des définitions de rôle dans RoleFactory et création de rôles dans RoleSeeder.

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -17,7 +17,8 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->jobTitle(),
+            'description' => $this->faker->sentence(),
         ];
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Role::factory()->create([
+            'name' => __('admin'),
+            'description' => 'Administrator with full access',
+        ]);
+
+        Role::factory()->create([
+            'name' => __('farmer'),
+            'description' => 'Farmer with limited access',
+        ]);
     }
 }
