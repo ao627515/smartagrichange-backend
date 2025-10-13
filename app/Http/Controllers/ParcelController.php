c2c0c698c58f8d6d8ecf8a31b8e302ba9a81dbf7
ao627515 <ao627515@gmail.com>
2025-10-13 13:21:07 +0000

    feat: Ajout des contrôleurs, modèles, usines, migrations et semeurs pour les ressources Field et Parcel

---
+++ app/Http/Controllers/ParcelController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Parcel;
+use Illuminate\Http\Request;
+
+class ParcelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Parcel $parcel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Parcel $parcel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Parcel $parcel)
+    {
+        //
+    }
+}
