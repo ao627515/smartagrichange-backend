69ac6588c9de286a7b00d5361b7b2117c97e573f
ao627515 <ao627515@gmail.com>
2025-10-28 21:52:42 +0000

    `Refactor HandleApiRequestException trait and update language file`

--- app/Traits/HandleApiRequestException.php
+++ app/Traits/HandleApiRequestException.php
@@ -2,17 +2,18 @@
 
 namespace App\Traits;
 
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Http\Resources\ErrorResponseResource;
 use App\Http\Resources\SuccessResponseResource;
-use Exception;
-use Illuminate\Support\Facades\Log;
 
 trait HandleApiRequestException
 {
     public function handleRequestException($callable): SuccessResponseResource|ErrorResponseResource
     {
         try {
-            return $callable();
+            return DB::transaction(fn() => $callable());
         } catch (Exception $e) {
             // Log the exception or handle it as needed
             Log::error('handleRequestException: ' . $e->getMessage());
